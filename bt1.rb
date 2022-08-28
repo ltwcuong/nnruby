d61cb6b1fa93188cf0c12983329c28dae4b7491f
ltwcuong <95215320+ltwcuong@users.noreply.github.com>
2022-08-28 20:07:56 +0700

    Delete bt1.rb

--- bt1.rb
+++
@@ -1,7 +0,0 @@
-puts "Họ tên :Nguyễn Thanh Cường MaSV: 1911505310204"
-puts "Nhập số tuỳ ý: "
-number = gets
-print "Số vừa nhập: #{number}"
-puts "Nhập hai số tuỳ ý: "
-a, b = gets.split
-puts "Số max : #{a>b ? a : b}" 
