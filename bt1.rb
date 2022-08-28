19259d30150a0ecb66ab0cc92baf03abb72db7b8
ltwcuong <95215320+ltwcuong@users.noreply.github.com>
2022-08-28 20:08:30 +0700

    Add files via upload

---
+++ bt1.rb
@@ -0,0 +1,7 @@
+puts "Xin chào,Nguyễn Thanh Cường,1911505310204"
+puts "Nhập số tuỳ ý: "
+number = gets
+print "Số vừa nhập: #{number}"
+puts "Nhập hai số tuỳ ý: "
+a, b = gets.split
+puts "Số max : #{a>b ? a : b}" 
